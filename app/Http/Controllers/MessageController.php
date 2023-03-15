3bb2a0f75a013284669945fcd619143e1e0b9aec
Tramaine Jackson <jackson.tramaine3@gmail.com>
2023-03-15 14:15:52 -0400

    Added new check before adding messages to database

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -10,29 +10,30 @@ use Carbon\Carbon;
 
 class MessageController extends Controller
 {
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth')->except('store');
-	}
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except('store');
+    }
 
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index() {
-    	$admin = Auth::user();
-	    $messagesAll = Message::all();
-	    $messages = Message::paginate(5);
-	    $today = Carbon::now();
+    public function index()
+    {
+        $admin = Auth::user();
+        $messagesAll = Message::all();
+        $messages = Message::paginate(5);
+        $today = Carbon::now();
 
 
-	    return view('messages', compact('admin', 'messages', 'messagesAll', 'today'));
+        return view('messages', compact('admin', 'messages', 'messagesAll', 'today'));
     }
 
     /**
@@ -40,48 +41,56 @@ class MessageController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create() {
+    public function create()
+    {
         //
     }
 
     /**
      * Send email from message left on about us page.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request) {
-	    // Validate incoming data
-	    $this->validate($request, [
-		    'message_email' => array(
-		    	'required',
-		    	'max:100',
-		    	'regex:/\.com|\.co|\.net$/',
-		    ),
-		    'message_name' => 'required|max:50',
-		    'message_subject' => 'required|max:100',
-		    'message_body' => 'required|max:500',
-	    ]);
+    public function store(Request $request)
+    {
+        // Validate incoming data
+        $this->validate($request, [
+            'message_email' => array(
+                'required',
+                'max:100',
+                'regex:/\.com|\.co|\.net$/',
+            ),
+            'message_name' => 'required|max:50',
+            'message_subject' => 'required|max:100',
+            'message_body' => 'required|max:500',
+        ]);
 
-	    $message            = new Message();
-	    $message->email     = $request->message_email;
-	    $message->name      = $request->message_name;
-	    $message->subject   = $request->message_subject;
-	    $message->message   = $request->message_body;
+        $message = new Message();
+        $message->email = $request->message_email;
+        $message->name = $request->message_name;
+        $message->subject = $request->message_subject;
+        $message->message = $request->message_body;
 
+        if ($message->funkyCheck() === false) {
+            $email_check = Message::where('email', $message->email)->get();
+            if ($email_check->isEmpty()) {
+                if ($message->save()) {
+                    // Send Email to Admin and Recipient
+                    //\Mail::to($request->message_email)->send(new NewMessage($request->message_subject, $request->message_name, $request->message_email, $request->message_body));
 
-	    if($message->save()) {
-		    // Send Email to Admin and Recipient
-//		    \Mail::to($request->message_email)->send(new NewMessage($request->message_subject, $request->message_name, $request->message_email, $request->message_body));
-
-		    return redirect()->back()->with('status', "Message Sent Successfully");
-	    }
+                    return redirect()->back()->with('status', "Message Sent Successfully");
+                }
+            }
+        } else {
+            return redirect()->back()->with('status', "Message Sent Successfully");
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -92,7 +101,7 @@ class MessageController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -103,8 +112,8 @@ class MessageController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -115,12 +124,13 @@ class MessageController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Message $message) {
-	    if($message->delete()) {
-		    return redirect()->action('MessageController@index')->with('status', 'Message Deleted Successfully!');
-	    }
+    public function destroy(Message $message)
+    {
+        if ($message->delete()) {
+            return redirect()->action('MessageController@index')->with('status', 'Message Deleted Successfully!');
+        }
     }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,40 +6,126 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-	/**
-	 * Create full name accessor
-	 *
-	 * @param \Illuminate\Database\Eloquent\Builder $query
-	 * @return mixed
-	 */
-	public function full_name()	{
-		return $this->first_name . " " . $this->last_name;
-	}
+    /**
+     * Create full name accessor
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return mixed
+     */
+    public function full_name()
+    {
+        return $this->first_name . " " . $this->last_name;
+    }
 
-	/**
-	 * Get the consult request record associated with the user.
-	 */
-	public function consultContact()	{
-		return $this->hasOne('App\ConsultContact');
-	}
+    /**
+     * Get the consult request record associated with the user.
+     */
+    public function consultContact()
+    {
+        return $this->hasOne('App\ConsultContact');
+    }
 
-	/**
-	 * Get the consult request record associated with the user.
-	 */
-	public function consultResponse()	{
-		return $this->hasOne('App\ConsultResponse');
-	}
+    /**
+     * Get the consult request record associated with the user.
+     */
+    public function consultResponse()
+    {
+        return $this->hasOne('App\ConsultResponse');
+    }
 
-	/**
-	 * Scope a query to only include most recent consult request
-	 * that hasn't been responded to yet.
-	 *
-	 * @param \Illuminate\Database\Eloquent\Builder $query
-	 * @return \Illuminate\Database\Eloquent\Builder
-	 */
-	public function scopeLeastRecent($query)	{
-		return $query->where('responded', '=', 'N')
-			->orderBy('created_at', 'asc')
-			->get();
-	}
+    /**
+     * Get the consult request record associated with the user.
+     */
+    public function funkyCheck()
+    {
+        $funkyCheck = false;
+
+        $skip_emails = [
+            '@course-fitness.com',
+            '2.83.51.s.b.9.xm@dynainbox.com',
+            'alspeersxmz7@outlook.com',
+            'info@needvirtualassistant.com',
+            'outreach@morelocalclients.net',
+            'no-replywheva@gmail.com',
+            'donayb7@sora5710.kaede39.officemail.in.net',
+            'donelleej13ter@outlook.com',
+            'frideamrik2@hotmail.com',
+            'friend@tour-med.com',
+            'c.h.a.r.med.c.rist.k.t.@gmail.com',
+            'g.allan.ta.tta.ck.er.@gmail.com',
+            'g.al.la.n.ta.t.tac.k.e.r@gmail.com',
+            'gallant.a.tt.ac.k.e.r@gmail.com',
+            'griskafishka234@gmail.com',
+            'hunterlmx6347@gmail.com',
+            'hvqsjdib@gmail.com',
+            'julianamk18@hikaru3910.hiraku97.officemail.in.net',
+            'kai.rus.sell009.8@gmail.com',
+            'kai.russell009.8@gmail.com',
+            'kostanboloyan@gmail.com',
+            'lidam3regrundy@outlook.com',
+            'pozvonochnik.od.ua@gmail.com',
+            'seller.amazon.web@gmail.com',
+            's.c.ot.tr.o.b.bi.nsd.brm.5u.@gmail.com',
+            'sc.ottro.b.b.ins.dbrm5.u.@gmail.com',
+            'timofeeffsergey83@gmail.com',
+            'webmaster@gameplayin.net',
+            'zelatcol@gmail.com',
+        ];
+        $skip_phrases = [
+            'porn',
+            'galleries',
+            'teens',
+            'pussy',
+            'sexy',
+            'sex',
+            'lose weight',
+            'attention',
+            'child',
+            'robot',
+            'make money',
+            'increase your financial',
+            'earn additional money',
+            'erection',
+            'fuck',
+            'playmate',
+            'nude',
+            'naked',
+            'need cash',
+            'need money',
+            'seduction',
+            'seduce',
+        ];
+
+        foreach ($skip_emails as $email) {
+            if(substr_count($this->email, $email) > 0) {
+                $funkyCheck = true;
+                break;
+            }
+        }
+
+        if($funkyCheck === false) {
+            foreach ($skip_phrases as $phrase) {
+                if((substr_count($this->subject, $phrase) > 0) || (substr_count($this->message, $phrase) > 0)) {
+                    $funkyCheck = true;
+                    break;
+                }
+            }
+        }
+
+        return $funkyCheck;
+    }
+
+    /**
+     * Scope a query to only include most recent consult request
+     * that hasn't been responded to yet.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeLeastRecent($query)
+    {
+        return $query->where('responded', '=', 'N')
+            ->orderBy('created_at', 'asc')
+            ->get();
+    }
 }
