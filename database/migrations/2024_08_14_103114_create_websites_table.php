1398414ae0380fca005cbced70ba240442dbd8d0
Tramaine Jackson <jackson.tramaine3@gmail.com>
2024-08-14 11:43:48 -0400

    Added new database table via migrate

---
+++ database/migrations/2024_08_14_103114_create_websites_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWebsitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name', 25)->nullable();
+            $table->string('title', 25)->nullable();
+            $table->text('description')->nullable();
+            $table->string('link', 100)->nullable();
+            $table->string('owner', 50)->nullable();
+            $table->string('owner_email', 100)->nullable();
+            $table->string('logo', 100)->nullable();
+            $table->date('renew_date')->nullable();
+            $table->date('last_paid_date')->nullable();
+            $table->double('amount_due', 15,2)->nullable();
+            $table->char('active', 1)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('websites');
+    }
+}
